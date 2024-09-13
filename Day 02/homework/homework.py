56197f22933ae2a809fcb1269eea4db2fd3e2a07
shar1kadzee <barbaresharikadze42@gmail.com>
2024-09-13 12:22:59 +0400

    commit

---
+++ Day 02/homework/homework.py
@@ -0,0 +1,30 @@
+score = 15
+health = 7
+
+print(score)
+print(health)
+
+
+attacking_points = 150
+defence_points = 777
+
+print(attacking_points)
+print(defence_points)
+
+math_score = 9
+english_score = 10
+science_score = 9
+chemistry_score = 10
+history_score = 8
+
+print(math_score)
+print(english_score)
+print(science_score)
+print(chemistry_score)
+print(history_score)
+
+name = "barbare"
+surname = "sharikadze"
+age = 14
+
+print(name,surname,age)
