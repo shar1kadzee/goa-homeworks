e9483f37ad749ddee58bdb2d906c9fd1de55fca4
shar1kadzee <barbaresharikadze42@gmail.com>
2024-09-13 08:51:15 +0400

    commit

---
+++ Day 01/homeworks/lesson1.py
@@ -0,0 +1,30 @@
+name="barbare"
+
+surname = "sharikadze"
+
+name = "barbare"  #ეს არის str (string) ტიპის ცვლადი
+age = 14 # ეს არის int (integer) მთელი რიცხვი
+height = 165.5 #ეს არის float ტიპის ცვლადი (ათწილადი) 
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing = True          #True ან False
+is_ugly = False     #snakecase ( წერის სტილი სტანდარტულად )
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+# print(name + " " + surname)
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programing))
+
+
+print(name + " " + str(age))
+
+print(surname + " " + str(age) )
+
+print(name + " " ,surname)
+
+print("me","var",name, "chemi gvaria",surname,"var",age,"wlis")
