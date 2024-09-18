d9f5158e61fee5151c5a19f96377bd40675a61dc
shar1kadzee <barbaresharikadze42@gmail.com>
2024-09-18 09:03:28 +0400

    commit

---
+++ Day 03/homework/lesson3.py
@@ -0,0 +1,33 @@
+# name = input("name:")
+# lastname = input("lastname:")
+# age = input("age:")
+
+# print(name, lastname, "aris" , age, "wlis")
+
+first = int(input("num1:"))
+second = int(input("num2:"))
+
+print(first + second)
+print(first - second)
+print(first * second)
+print(first / second)
+
+user = input("enter your name: ")
+print(user)
+
+math = 20 + 20
+math = math - 10
+math = math * 2
+print = math
+
+academy = input("academy:")
+school = input("school:")
+name = input("name:")
+lastname = input("lastname:")
+age = input("age:")
+
+print(academy)
+print(school)
+print(name)
+print(lastname)
+print(age)
