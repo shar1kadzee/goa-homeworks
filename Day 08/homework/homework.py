bd20731497b97f92ebab1dbde8ecd7d8c5ba0f57
shar1kadzee <barbaresharikadze42@gmail.com>
2024-10-05 11:50:49 +0400

    commit

---
+++ Day 08/homework/homework.py
@@ -0,0 +1,79 @@
+# age = int(input("asaki"))
+# print(13 < 15)
+# print(15 < 19 )
+
+
+# num1 = int(input("rickhvi"))
+# is_a = (7 > 9)
+# is_b = (7 >= 8)
+# is_c = (7 >= 7)
+# is_d = (7 >= 6)
+# is_f = (7 < 6)
+
+# print(is_a)
+# print(is_b)
+# print(is_c)
+# print(is_d)
+# print(is_f)
+
+
+
+# True = 2 == 2
+# False = 1 != 3
+
+# print(true or true)
+# print(false or true)
+# print(false or false)
+# print(true or false)
+# print(false and true)
+# print(false and false)
+# print(true and true)
+# print(true and false)
+
+
+# num1 = int(input("raime rickhvi"))
+# num2 = int(input("raime rickhvi"))
+
+# print(num1 > num2)
+# print(num1 < num2)
+# print(num1 >= num2)
+# print(num1 <= num2)
+# print(num1 == num2)
+# print(num1 != num2)
+
+
+
+# a = 2
+# b = 1
+# c = 3
+# is_a_greatest = 2 > 3
+# is_b_middle = 1 > 3 
+# is_c_least = 3 > 2
+
+# print(is_a_greatest)
+# print(is_b_middle)
+# print(is_c_least)
+
+
+# score = 80
+# is_pass = (80 >= 50)
+# is_high_pass = (75 < 80 < 90)
+# is_perfect = (100 == 80)
+# is_failing = (50 > 80)
+
+# print(is_pass)
+# print(is_high_pass)
+# print(is_perfect)
+# print(is_failing)
+
+
+
+p = 1 > 2
+q = 2 > 1
+P_and_not_Q = (p > q)
+not_P_and_Q = (p < q)
+P_xor_Q = (p or q)
+
+print(P_and_not_Q)
+print(not_P_and_Q)
+print(P_xor_Q)
