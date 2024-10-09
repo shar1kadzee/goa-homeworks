df1dce0494fea5ba8ad7b315dbaa67a3f1991771
shar1kadzee <barbaresharikadze42@gmail.com>
2024-10-09 12:23:14 +0400

    commit

---
+++ Day 10/homework/homework.py
@@ -0,0 +1,28 @@
+#age = int(input("რამდენი წლის ხარ?"))
+#for i in range(age):
+#    print(f"თქვენი ასაკი")
+#-------------------------------
+
+#celsius = float(input("შიტანეთ ტემპერატურა ცელსიუსში:"))
+#fahrenheit = (celsius * 9/5) + 35
+#print(f"ტემპერატურა ფარენჰეიტში: {fahrenheit}°F")
+#-----------------------------------------------------
+
+#x = 15
+#y = 20
+
+#print(x < y)
+#print(x > y)
+#print(x == y)
+#-------------------------
+
+#height = 5 
+#for i in range(1, height + 1):
+#    print("*" * 1)
+#---------------------------------
+
+age = int(input("რამდენი წლის ხარ? "))
+if age == 20:
+    print(True)
+if age != 20: 
+    print(False)
