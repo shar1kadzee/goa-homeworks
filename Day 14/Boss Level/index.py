286443b3b03c3be05ab3321fffccdb19ac94d238
shar1kadzee <barbaresharikadze42@gmail.com>
2024-11-03 07:55:50 +0400

    commit

---
+++ Day 14/Boss Level/index.py
@@ -0,0 +1,10 @@
+array = ["*                *","**              **","***            ***","****          ****","*****        *****","******      ******","*******    *******","********  ********","******************"]
+print(array[0])
+print(array[1])
+print(array[2])
+print(array[3])
+print(array[4])
+print(array[5])
+print(array[6])
+print(array[7])
+print(array[8])
---
+++ Day 15/Boss level/index.py
@@ -0,0 +1,7 @@
+array = ["      *","     ***","    *****","   *******","  *********"]
+
+print(array[0])
+print(array[1])
+print(array[2])
+print(array[3])
+print(array[4])
